6f9976fe53c67bcc5c4c56d6e7704908a871e45f
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-08-16 13:43:29 -0300

    feat: add  templates location for view engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,16 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirPath = path.join(__dirname, '../public')
 
+// Define paths for express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
