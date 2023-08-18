522ac8b3d262405a4f3539f7d58b3fd31f315a94
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-08-17 22:00:32 -0300

    feat: add client side weather request

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,25 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-  response.json().then((data) => {
-    console.log(data)
-  })
-})
+const weatherForm = document.querySelector('form')
+const secondMessage = document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-  response.json().then((data) => {
-    console.log(data)
-  })
+const getWeather = (location) => {
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          secondMessage.textContent = data.error
+        } else {
+          const content = `${data.weather} in ${data.location}`
+          secondMessage.textContent = content
+        }
+      })
+    }
+  )
+}
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const { search } = event.target
+  getWeather(search.value)
 })
