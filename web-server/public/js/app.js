aa769e979cd76d0764c9f59cfa8af9448b7ecd54
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-08-20 22:24:56 -0300

    feat: add request by current address

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -4,18 +4,16 @@ const weatherForm = document.querySelector('form')
 const secondMessage = document.querySelector('#message-2')
 
 const getWeather = (location) => {
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          secondMessage.textContent = data.error
-        } else {
-          const content = `${data.weather} in ${data.location}`
-          secondMessage.textContent = content
-        }
-      })
-    }
-  )
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        secondMessage.textContent = data.error
+      } else {
+        const content = `${data.weather} in ${data.location}`
+        secondMessage.textContent = content
+      }
+    })
+  })
 }
 
 weatherForm.addEventListener('submit', (event) => {
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -88,6 +89,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, (listen) => {
-  console.log('http://localhost:3000/')
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`)
 })
